eca1f0937fe009b78927e70adb4085135e020fc0
Anuj Yadav <73534589+ajishere07@users.noreply.github.com>
2025-12-19 15:17:41 +0530

    Implemented LinkedList Linear Search

--- Arrays/Algorithms/LinearSearch.java
+++ Arrays/Algorithms/LinearSearch.java
@@ -21,3 +21,78 @@ public class Main {
       }
   }
 }
+
+
+/*---------------------------------------------------Linked list --------------------------------------------------*/
+
+  public class LinkedList{
+    Node head;
+    
+    public LinkedList(){
+      this.head= null;
+    }
+    
+    void addNode(Node newNode){
+      
+      if(head == null){
+        head = newNode;
+        return;
+      }
+      Node current = head;
+      while(current.next!=null){
+         current = current.next ;
+      }
+      
+      current.next  = newNode;
+    }
+    
+    boolean linearSearch(int target){
+      if(head == null){
+        return false;
+      }
+      Node current = head;
+      
+      while(current!=null){
+        if(current.val == target){
+          return true;
+        }
+        current = current.next;
+      }
+      
+      return false;
+    }
+}
+
+public class Node{
+  int val;
+  Node next;
+  
+  public Node(int val){
+    this.val = val;
+    this.next = null;
+  }
+} 
+
+public class Main {
+    public static void main(String[] args) {
+       Node n1 = new Node(3);
+         Node n2 = new Node(5);
+           Node n3 = new Node(7);
+           
+           
+           LinkedList list =  new LinkedList();
+           list.addNode(n1);
+           list.addNode(n2);
+           list.addNode(n3);
+           Node current = list.head;
+           while(current!= null){
+             System.out.print(current.val + " ");
+             current = current.next;
+           }
+           
+           System.out.print(list.linearSearch(7));
+  }
+}
+
+
+
