74358027c89397c0279a1dd51bf101f7fc60be34
Anuj Yadav <73534589+ajishere07@users.noreply.github.com>
2025-12-23 00:12:48 +0530

    find the target indices and return arraylist
    Added a method to find indices of a target in an array.

--- Recursion/recursion_array_practice.java
+++ Recursion/recursion_array_practice.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
       int[] arr = {3,9,6,7,9};
+      int[] arr2 = {3,9,6,7,7,9,7,7};
       
       System.out.print(isSorted(arr,0));
+
+      ArrayList<Integer> list = index(arr2,0,7,new ArrayList<>());
+      
+      for(int num: list){
+        System.out.print(num);
+      }
   }
   
   static boolean isSorted(int[] arr,int index){
     if(index == arr.length-1) return true; 
     return arr[index] < arr[index+1] && isSorted(arr, index+1);
+  }
+
+  static ArrayList<Integer> index(int[] arr, int index, int target ,ArrayList<Integer> list){
+     if(index == arr.length) return list;
+     if(arr[index] == target) list.add(index);
+     
+     return index(arr,index+1,target,list);
   }
 }
