437367a1f8ccfb4e44cfc1c0eeb698970124a065
Anuj Yadav <73534589+ajishere07@users.noreply.github.com>
2025-12-25 03:17:43 +0530

    Implement recursive selection sort method
    Added a recursive implementation of selection sort.

--- Arrays/Algorithms/SelectionSort.java
+++ Arrays/Algorithms/SelectionSort.java
@@ -36,5 +36,20 @@ public class Main {
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
-}
+  }
+
+  //Recursive selection sort
+  static void selectionSort(int[] arr, int r, int c, int max){
+      if(r==0) return;
+      
+      if(c<r) {
+        if(arr[c] > arr[max]) selectionSort(arr,r,c+1,c);
+        else selectionSort(arr,r,c+1,max);
+      }else {
+        int temp = arr[max];
+        arr[max] = arr[r-1];
+        arr[r-1] = temp;
+        selectionSort(arr, r-1, 0, 0);
+      }
+    }  
 }
